f71fe845aaadc4b5d0551117d98424f1a51eb16a
nuaazs <zhaosheng@nuaa.edu.cn>
2023-07-28 10:37:13 +0800

    add encrypt

--- text_classification/egs/easy_tc_fraud/test_encrypt_data.py
+++ text_classification/egs/easy_tc_fraud/test_encrypt_data.py
@@ -1,6 +1,7 @@
 from dguard_nlp.utils.utils import encrypt_compress_file
 from dguard_nlp.utils.utils import decrypt_decompress_file
 import os
-encrypt_compress_file('/home/zhaosheng/bert_fraud_classify/text_classification/data/weibo_senti_100k_sentence.csv', 'output.bin', b'passwordpassword')
+filepath = '/home/zhaosheng/bert_fraud_classify/text_classification/data/weibo_senti_100k_sentence.csv'
+encrypt_compress_file(filepath, 'output.bin', b'passwordpassword')
 decrypt_decompress_file('output.bin', 'output.txt', b'passwordpassword')
 os.system('rm output.bin')
