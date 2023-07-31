204bc045285222fda93be49bdd9883b2367c3adf
nuaazs <zhaosheng@nuaa.edu.cn>
2023-07-31 17:07:55 +0800

    add am-softmax aam-softmax etc.

--- text_classification/data/shuffle_csv.sh
+++ text_classification/data/shuffle_csv.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
 # 读取文件内容，并将第一行保存到变量header中
-header=$(head -n 1 private_st.csv)
+header=$(head -n 1 16w_data.csv)
 
 # 打乱文件内容（除第一行外）
-tail -n +2 private_st.csv | shuf > temp.csv
+tail -n +2 16w_data.csv | shuf > temp.csv
 
 # 将打乱后的内容与第一行合并为新的文件output.txt
-echo "$header" > output.csv
-cat temp.csv >> output.csv
+echo "$header" > 16w_formated.csv
+cat temp.csv >> 16w_formated.csv
 
 # 去除文件中的中文标点符号
-sed -i 's/[，。？！【】《》（）：；]//g' output.csv
+sed -i 's/[，。？！【】《》（）：；]//g' 16w_formated.csv
 
 # 删除临时文件
 rm temp.csv
