5594706d2287fee660c86cf6250c5debf2b0b348
nuaazs <zhaosheng@nuaa.edu.cn>
2023-08-02 13:15:15 +0800

    add label smooth

--- text_classification/egs/easy_tc_fraud/main_single.py
+++ text_classification/egs/easy_tc_fraud/main_single.py
@@ -6,34 +6,54 @@ from torch.nn.parallel import DistributedDataParallel as DDP
 from dguard_nlp.utils import load_yaml2dict
 from dguard_nlp.utils.seed import set_seed
 from dguard_nlp.models.bert import BertClassificationModel
+from dguard_nlp.models.classify import CosineClassifier
 from dguard_nlp.optimizer.optimizers import get_optimizer
 from dguard_nlp.datasets.preprocess import get_dataframe
 from dguard_nlp.bin.train import train
 from dguard_nlp.bin.test import test
-
+from dguard_nlp.utils import get_logger
+from dguard_nlp.utils.config import build_config
+from dguard_nlp.utils.builder import build
+from dguard_nlp.utils.config import Config
 import argparse
 parser = argparse.ArgumentParser(description='')
 parser.add_argument('--config', type=str, default='conf/config.yaml',help='')
 args = parser.parse_args()
-torch.cuda.set_device("cuda:0")
-gpu="cuda:0"
-set_seed(100)
 
 if __name__ == '__main__':
     # args get config.yaml path
     args = parser.parse_args()
     config = load_yaml2dict(args.config)
-
+    set_seed(config['seed'])
+    if config['device'] != "cpu":
+        torch.cuda.set_device(config['device'])
     train_loader, vali_loader, test_loader = get_dataframe(config)
-    print(f"Load data done! Train: {len(train_loader)} Vali: {len(vali_loader)} Test: {len(test_loader)}")
+    exp_name = config['exp']
+    exp_path = f'./exp/{exp_name}'
+    os.makedirs(exp_path, exist_ok=True)
+    # save config to exp_path
+    with open(f'{exp_path}/config.yaml', 'w') as f:
+        yaml.dump(config, f)
 
-    model = BertClassificationModel().to(gpu)
+    # add logger, save to exp_path/train.log and print to console
+    logger = get_logger(f'{exp_path}/train.log')
+    logger.info(f"Load data done! Train: {len(train_loader)} Vali: {len(vali_loader)} Test: {len(test_loader)}")
 
-    print(f"Model has {sum(p.numel() for p in model.parameters() if p.requires_grad):,} trainable parameters")
-    print(f"Device: {gpu}")
+    model = BertClassificationModel(device=config['device'])
+    cos_model = CosineClassifier(input_dim=768,num_blocks=3,inter_dim=512,out_neurons=2).to(config['device'])
+    pipeline = torch.nn.Sequential(model,cos_model)
+    
+    logger.info(f"Model has {sum(p.numel() for p in pipeline.parameters()):,} parameters")
+    logger.info(f"Model has {sum(p.numel() for p in pipeline.parameters() if p.requires_grad):,} trainable parameters")
+    logger.info(f"Device: {config['device']}")
+    # optimizer = #get_optimizer(config, pipeline)
+    
+    config_class = Config(config)
+    optimizer = build('optimizer', config_class)
+    config_class.lr_scheduler['args']['step_per_epoch'] = len(train_loader)
+    # print(config["lr_scheduler"])
+    lr_scheduler = build('lr_scheduler', config_class)
 
-    optimizer = get_optimizer(config, model)
-
-    model, optimizer = train(config['epochs'], config['valid_interval'], model, optimizer, train_loader, vali_loader,device=gpu)
-    r = test(model, test_loader)
+    model, optimizer = train(config, pipeline, optimizer, lr_scheduler,train_loader, vali_loader,logger=logger,device=config['device'])
+    r = test(pipeline, test_loader)
     print(f">>> The accuracy of the model on the test set is: {r * 100:.2f}%")
--- text_classification/egs/easy_tc_fraud/test_encrypt_data.py
+++ text_classification/egs/easy_tc_fraud/test_encrypt_data.py
@@ -2,6 +2,6 @@ from dguard_nlp.utils.utils import encrypt_compress_file
 from dguard_nlp.utils.utils import decrypt_decompress_file
 import os
 filepath = '/home/zhaosheng/bert_fraud_classify/text_classification/data/weibo_senti_100k_sentence.csv'
-encrypt_compress_file(filepath, 'output.bin', b'passwordpassword')
-decrypt_decompress_file('output.bin', 'output.txt', b'passwordpassword')
-os.system('rm output.bin')
+# encrypt_compress_file(filepath, 'output.bin', b'passwordpassword')
+# decrypt_decompress_file('output.bin', '14w.csv', b'passwordpassword')
+# os.system('rm output.bin')
