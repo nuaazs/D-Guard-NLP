1fe5e8202d7f190d3464b5573dfcbbcc20c93783
nuaazs <zhaosheng@nuaa.edu.cn>
2023-08-07 15:08:29 +0800

    add acc recall precision for test

--- dguard_nlp/bin/test.py
+++ dguard_nlp/bin/test.py
@@ -12,7 +12,17 @@ def test(model,test_loader):
             out=model(data) # [batch_size,num_class]
 
         out = out.argmax(dim=1)
-        correct += (out.cpu() == labels).sum().item()
-        total += len(labels)
-    print(f">>> The accuracy of the model on the test set is: {correct / total * 100:.2f}%")
+        # correct += (out.cpu() == labels).sum().item()
+        # total += len(labels)
+        tn =   ((out.cpu() == 0) & (labels == 0)).sum().item()
+        tp =   ((out.cpu() == 1) & (labels == 1)).sum().item()
+        fn =   ((out.cpu() == 0) & (labels == 1)).sum().item()
+        fp =   ((out.cpu() == 1) & (labels == 0)).sum().item()
+    acc_test = (tn+tp)/(tn+tp+fn+fp)
+    recall_test = tp/(tp+fn)
+    precision_test = tp/(tp+fp)
+    # print(f">>> The accuracy of the model on the test set is: {correct / total * 100:.2f}% \n")
+    print(f">>> The accuracy of the model on the test set is: {acc_test * 100:.2f}% \n")
+    print(f">>> The recall of the model on the test set is: {recall_test * 100:.2f}% \n")
+    print(f">>> The precision of the model on the test set is: {precision_test * 100:.2f}% \n")
     return correct / total * 100
